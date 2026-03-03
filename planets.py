342a0d88f1f6c58d4450be14587e20f5c2534de4
Jack Gardner <jtgardner99@gmail.com>
2026-03-03 16:05:00 -0600

    initial commit

--- planets.py
+++ planets.py
@@ -32,3 +32,45 @@ print(planet_list)
 del planet_list[8]
 
 print(planet_list)
+
+spacecraft_list = [
+    ("Mariner 10", "Mercury", "Venus"),
+    ("MESSENGER", "Mercury", "Venus"),
+    ("Venera 1-16", "Venus"),
+    ("Mariner 2", "Venus"),
+    ("Mariner 5", "Venus"),
+    ("Pioneer Venus 1 and 2", "Venus"),
+    ("Vega 1 and 2", "Venus"),
+    ("Magellan", "Venus"),
+    ("Venus Express", "Venus"),
+    ("Parker Solar Probe", "Venus"),
+    ("Mariner 4", "Mars"),
+    ("Mariner 6 and 7", "Mars"),
+    ("Mariner 9", "Mars"),
+    ("Viking 1 and 2", "Mars"),
+    ("Mars Pathfinder", "Mars"),
+    ("Mars Odyssey", "Mars"),
+    ("Spirit", "Mars"),
+    ("Opportunity", "Mars"),
+    ("Phoenix", "Mars"),
+    ("Dawn", "Mars"),
+    ("Curiosity", "Mars"),
+    ("InSight", "Mars"),
+    ("Perseverance", "Mars"),
+    ("Pioneer 10 and 11", "Jupiter"),
+    ("Voyager 1 and 2", "Jupiter", "Saturn", "Uranus", "Neptune"),
+    ("Ulysses", "Jupiter"),
+    ("Galileo", "Venus", "Jupiter"),
+    ("Cassini", "Venus", "Jupiter", "Saturn"),
+    ("New Horizons", "Jupiter", "Pluto"),
+    ("Juno", "Jupiter"),
+    ("Pioneer 11", "Jupiter", "Saturn"),
+]
+
+for craft in spacecraft_list:
+    spacecraft = craft[0:1]
+    planets = craft[1:]
+    print(f"{spacecraft[0]} has visited")
+    print("-------------------")
+    for planet in planets:
+        print(f"{planet}")
